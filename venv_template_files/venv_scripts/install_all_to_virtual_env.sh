c373f92bde68ab0778fdb0caa97b3233b8e5c47c
Hugh McGuirk <20146038+hmcguirk@users.noreply.github.com>
2021-01-21 14:33:04 +0000

    some missed file udpates

--- venv_template_files/venv_scripts/install_all_to_virtual_env.sh
+++ venv_template_files/venv_scripts/install_all_to_virtual_env.sh
@@ -11,9 +11,11 @@ fi
 
 echo Installing all required packages
 
+pip3 install wheel
 pip3 install --upgrade tensorflow
 pip3 install keras
 pip3 install tensorflow-addons
+pip3 install tensorflow_hub
 pip3 install keras-tuner
 pip3 install --upgrade matplotlib
 pip3 install filesplit
--- venv_template_files/venv_scripts/install_system_prerequisites.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo Installing all system prerequisites \(These are not part of any virtual environment\)
-
-# ERROR: launchpadlib 1.10.13 requires testresources, which is not installed.
-sudo apt install python3-testresources
